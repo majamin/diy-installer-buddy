d34f35f1c8c68d6d20ff66f3f83698963f8d2c71
Marian Minar <majamin@gmail.com>
2024-12-31 12:27:01 -0800

    add gh-pages, fix basePath

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const dev = process.env.NODE_ENV === "development";
-const basePath = "/REPO_NAME"; // Replace with your GitHub repo name
+const basePath = "/diy-installer-buddy"; // Replace with your GitHub repo name
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
