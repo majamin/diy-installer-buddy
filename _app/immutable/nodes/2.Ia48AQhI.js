ca52bd5b6f49380af516f0dbf33e8ce38f87fc16
Marian Minar <majamin@gmail.com>
2024-12-31 12:19:40 -0800

    Updates

---
+++ _app/immutable/nodes/2.Ia48AQhI.js
@@ -0,0 +1,2 @@
+import{a as p,t as l}from"../chunks/legacy.DY7PJ-QD.js";import{p as _,i as e,j as d,t as m,k as c,l as f,n as u,r as v}from"../chunks/runtime.sNbnHdCN.js";import{g as h,s as g}from"../chunks/index.Dere4Y3v.js";import{i as b}from"../chunks/lifecycle.DMTzM0RM.js";var k=l(`<h1>Welcome to SvelteKit</h1> <p>Visit <a>svelte.dev/docs/kit</a> to read the
+  documentation</p>`,1);function S(s,i){_(i,!1);const r=h(),[n,x]=r;b();var t=k(),a=e(d(t),2),o=e(f(a));m(()=>g(o,"href",n("https://svelte.dev/docs/kit",void 0))),u(),v(a),p(s,t),c()}export{S as component};
