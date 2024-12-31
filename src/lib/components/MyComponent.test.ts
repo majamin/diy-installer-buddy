6a07708042f54e2cd77ee956373502e9994844fa
Marian Minar <majamin@gmail.com>
2024-12-31 12:07:56 -0800

    add prettier formatting

--- src/lib/components/MyComponent.test.ts
+++ src/lib/components/MyComponent.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('MyComponent', () => {
-  it('should pass this test', () => {
+describe("MyComponent", () => {
+  it("should pass this test", () => {
     expect(true).toBe(true);
   });
 });
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { i18n } from "$lib/i18n";
-	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
-	import '../app.css';
-	let { children } = $props();
+  //export const prerender = true;
+  import { i18n } from "$lib/i18n";
+  import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+  import "../app.css";
+  let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	{@render children()}
+  {@render children()}
 </ParaglideJS>
